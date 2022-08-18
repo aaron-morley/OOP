e28ed4390b893051334b11e9dc54abf6e060a54e
aaron-morley <111508607+aaron-morley@users.noreply.github.com>
2022-08-18 23:39:59 +0930

    Add files via upload

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -1,17 +1,9 @@
 // main-2-1.cpp
 
 #include <iostream>
+#include <function-2-1.cpp>
 using namespace std;
 
-void multiples_of_seven(int *nums, int length) {
-    int *p = nums;
-	
-    for(;p < nums+length; p++){
-          if(*p % 7 == 0) {
-              cout << *p << endl;
-          }
-    }
-}
 
 int main() {
 	int nums[16] = {-7,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14};
