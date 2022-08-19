ce893d625846cb6d248cb4fa40ee6f546aca7f7b
aaron-morley <111508607+aaron-morley@users.noreply.github.com>
2022-08-19 22:25:33 +0930

    Add files via upload

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,15 +1,11 @@
 // function-1-1.cpp    
 	
 	
-	struct str {
-        string s;
-    };
-    
-   const int rows = 3;
-   const int columns = 2;
-   std::string a[rows][columns] = { };
-   std::string b[rows][columns] = { };
-
 void string_2d_copy(std::string first[][2], std::string second[][2], int n) {
-    std::copy(a[0], a[3]+1, b[0]);
+    //exit(0);
+    for (int index = 0; index < n; index++) {
+        //std::copy(&first[n][0], &first[n][1], &second[n][0]);
+        second[index][0] = first[index][0];
+        second[index][1] = first[index][1];
+    }
 }
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,19 +1,25 @@
 // main-1-1.cpp
 
 #include <iostream>
-using namespace std;
 #include "function-1-1.cpp"
+using namespace std;
 
+const int rows = 3;
 
 int main()
 {
-   str a[rows][columns] = { {"a", "b"}, {"c", "d"}, {"e", "f"} };
-   
-   string_2d_copy(std::string first, std::string second, n);
-   
-   for (int i=0; i<3; i++)
-		for (int j=0; j<2; j++) {
-			cout << "b[" <<i<< "][" <<j<< "]: ";
-			cout << b[i][j] << endl;
-		}
+    std::string a[rows][2] = { {"a", "b"}, {"c", "d"}, {"e", "f"} };
+    std::string b[rows][2] = {};
+
+
+
+   string_2d_copy(a, b, rows);
+
+
+
+   for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 2; j++) {
+            cout << "b[" << i << "][" << j << "]: ";
+            cout << b[i][j] << endl;
+        }
 }
