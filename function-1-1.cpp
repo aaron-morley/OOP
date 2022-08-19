d2d80792e2861aab1ba76d98727af08e93ecb0ee
aaron-morley <111508607+aaron-morley@users.noreply.github.com>
2022-08-19 22:29:19 +0930

    Add files via upload

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,10 +1,10 @@
 // function-1-1.cpp    
 	
+#include <string>
 	
 void string_2d_copy(std::string first[][2], std::string second[][2], int n) {
-    //exit(0);
+
     for (int index = 0; index < n; index++) {
-        //std::copy(&first[n][0], &first[n][1], &second[n][0]);
         second[index][0] = first[index][0];
         second[index][1] = first[index][1];
     }
